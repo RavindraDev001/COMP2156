bae22e1ca46e68aae85bc1cfed4d2a6852719f76
Ravindra <Ravindra.Ramharack@georgebrown.ca>
2023-02-10 18:30:21 -0500

    added project dependencies to our package.json

---
+++ build/precache-manifest.a47ee974b3580675ed79c7f4df67995b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3a3b5661701d07e0e9c",
+    "url": "/RavindraDev001/COMP2156/static/css/main.f67481a5.chunk.css"
+  },
+  {
+    "revision": "b3a3b5661701d07e0e9c",
+    "url": "/RavindraDev001/COMP2156/static/js/main.b3a3b566.chunk.js"
+  },
+  {
+    "revision": "f30cf3fbdca9194f13af",
+    "url": "/RavindraDev001/COMP2156/static/js/runtime~main.f30cf3fb.js"
+  },
+  {
+    "revision": "64d6decb088646398d8f",
+    "url": "/RavindraDev001/COMP2156/static/js/2.64d6decb.chunk.js"
+  },
+  {
+    "revision": "c5749ea135ff0270a07f0c9fd1db911b",
+    "url": "/RavindraDev001/COMP2156/index.html"
+  }
+];
